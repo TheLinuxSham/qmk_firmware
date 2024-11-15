4d98c1d1feb0c0330ef482f0a777a6355848399b
Jan Schneider <j.schn@tutanota.com>
2024-11-15 10:49:49 +0100

    update keys in layers and switched up

--- keyboards/bluebell/swoop/keymaps/custom/keymap.c
+++ keyboards/bluebell/swoop/keymaps/custom/keymap.c
@@ -56,62 +56,59 @@ void layer1_reset(tap_dance_state_t *state, void *user_data);
 
 // Define Layouts
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/*
- * BASE
+/* Base Layer
  * ,---------------------------------.     ,----------------------------------.
- * |  Q  |   W  |   E  |   R  |   T  |     |   Z  |   U  |   I  |   O  |   P  |
- * |-----+------+------+------+------|     |------+------+------+------+------|
- * |  A  |   S  |   D  |   F  |   G  |     |   H  |   J  |   K  |   L  |   ;  |
- * |-----+------+------+------+------|     ,------+------+------+------+------|
- * |  Y  |   X  |   C  |   V  |   B  |     |   N  |   M  |   ,  |   .  |   /  |
- * `------------+------+------+------|     |------+------+------+-------------'
- *              | Esc  | L1   | Tab  |     | Enter | Spc  | BSpc |
- *              `--------------------'     `--------------------'
+ * |  Q  |  W  |  E  |  R  |  T  |        |  Z  |  U  |  I  |  O  |  P  |
+ * |-----+-----+-----+-----+------|       |-----+-----+-----+-----+-----|
+ * |  A  |  S  |  D  |  F  |  G  |        |  H  |  J  |  K  |  L  |  ;  |
+ * |-----+-----+-----+-----+------|       |-----+-----+-----+-----+-----|
+ * |  Y  |  X  |  C  |  V  |  B  |        |  N  |  M  |  ,  |  .  |  /  |
+ * `-----------+-----+-----+------|       |-----+-----+-----+------------'
+ *             | Ctrl | BSPC| Esc |       | Enter| SPC | Alt |
+ *             `-------------------'       `-----------------'
  */
 [_BASE] = LAYOUT_split_3x5_3(
-    KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    KC_A,   KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-    MT(MOD_LSFT, KC_Z),   KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  MT(MOD_RSFT, KC_SLSH),
+    KC_Q, KC_W, KC_E, KC_R, KC_T,    KC_Y, KC_U, KC_I, KC_O, KC_P,
+    KC_A, KC_S, KC_D, KC_F, KC_G,    KC_H, KC_J, KC_K, KC_L, KC_SCLN,
+    MT(MOD_LSFT, KC_Z), KC_X, KC_C, KC_V, KC_B,    KC_N, KC_M, KC_COMM, KC_DOT, MT(MOD_LSFT, KC_SLSH),
                        MT(MOD_LALT, KC_ESC),  KC_LGUI,  MT(MOD_LCTL, KC_TAB),      TD(SPC_LAYR1), TD(ENT_LAYR2),  KC_BSPC
 ),
 
-/*
- * LAYER 1
+/* Layer 1
  * ,---------------------------------.     ,----------------------------------.
- * |  1  |   2  |   3  |   4  |   5  |     |   6  |   7  |   8  |   9  |   0  |
- * |-----+------+------+------+------|     |------+------+------+------+------|
- * |  `  |      |      |      |      |     | Left | Down |  Up  | Rght |  '   |
- * |-----+------+------+------+------|     ,------+------+------+------+------|
- * |     |      |      |      |      |     |   -  |   =  |   [  |   ]  |   \  |
- * `------------+------+------+------|     |------+------+------+-------------'
- *              | TRNS | TRNS |      |     |      |      |      |
- *              `--------------------'     `--------------------'
+ * |  1  |  2  |  3  |  4  |  5  |        |  6  |  7  |  8  |  9  |  0  |
+ * |-----+-----+-----+-----+------|       |-----+-----+-----+-----+-----|
+ * |  `  | End | PgUp| PgDn| Home |        |    |  -  |  =  |  [  |  ]  |
+ * |-----+-----+-----+-----+------|       |-----+-----+-----+-----+-----|
+ * | \|  |AltR |  @  |     |     |        |     |     |     |     |INT3 |
+ * `-----------+-----+-----+------|       |-----+-----+-----+------------'
+ *             | TRNS | TRNS| TRNS|       |  TD1| TRNS| TRNS|
+ *             `-------------------'       `-----------------'
  */
 [_LAYER1] = LAYOUT_split_3x5_3(
-    KC_1,   KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_GRV, KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
-    KC_LSFT,  KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-                       KC_TRNS, KC_TRNS, KC_LCTL,       TD(SPC_LAYR1),   KC_RIGHT_ALT,   KC_NO
+    KC_1, KC_2, KC_3, KC_4, KC_5,                           KC_6, KC_7, KC_8, KC_9, KC_0,
+    KC_GRV, KC_END, KC_PGUP, KC_PGDN,KC_HOME,               KC_QUOT, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC,
+    MT(MOD_LSFT, KC_NUBS), KC_RALT, KC_AT, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, MT(MOD_LSFT, KC_INT3),
+                  KC_TRNS, KC_TRNS, KC_TRNS,                TD(SPC_LAYR1), KC_TRNS, KC_TRNS
 ),
 
-/*
- * LAYER 2
+/* Layer 2
  * ,---------------------------------.     ,----------------------------------.
- * | F1  |  F2  |  F3  |  F4  |  F5  |     |  F6  |  F7  |  F8  |  F9  |  F10 |
- * |-----+------+------+------+------|     |------+------+------+------+------|
- * | F11 |  F12 |      |      |      |     | MRWD | VOLD | VOLU | MFFD | MUTE |
- * |-----+------+------+------+------|     ,------+------+------+------+------|
- * |     |      |      |      |      |     |      |      |      |      |      |
- * `------------+------+------+------|     |------+------+------+-------------'
- *              |      |      |      |     |      |      |      |
- *              `--------------------'     `--------------------'
+ * | F1  | F2  | F3  | F4  | F5  |        | F6  | F7  | F8  | F9  | F10 |
+ * |-----+-----+-----+-----+------|       |-----+-----+-----+-----+-----|
+ * | F11 | F12 |BrUp |BrDn |PSCR |        |LEFT |DOWN | UP  |RGHT |     |
+ * |-----+-----+-----+-----+------|       |-----+-----+-----+-----+-----|
+ * |Shift|     |     |     |     |        |Prev |Vol- |Vol+ |Next |Mute |
+ * `-----------+-----+-----+------|       |-----+-----+-----+------------'
+ *             | TRNS | TRNS| TRNS|       | TRNS|  TD2| TRNS|
+ *             `-------------------'       `-----------------'
  */
 [_LAYER2] = LAYOUT_split_3x5_3(
-    KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    KC_F11, KC_F12,  KC_NO,   KC_NO,   KC_NO,       KC_MRWD, KC_VOLD, KC_VOLU, KC_MFFD, KC_MUTE,
-    RCS(KC_NO), KC_NO, KC_NO, KC_NO, KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                       KC_NO,    KC_NO,   KC_NO,       KC_NO,   TD(ENT_LAYR2),   KC_NO
-)
+    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,    KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
+    KC_F11, KC_F12, KC_BRIU, KC_BRID, KC_PSCR, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO,
+    KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MUTE,
+                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TD(ENT_LAYR2), KC_TRNS
+    )
 };
 
 // Define Tapdance behavior
